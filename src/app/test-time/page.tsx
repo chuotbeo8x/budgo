c54454906c30ccfd3cd578ff3d0c151bc8a80b23
chuotbeo8x <hoanggiap@gmail.com>
2025-10-01 02:53:01 +0700

    Cleanup: remove debug files, test pages, unused components, and documentation

--- src/app/test-time/page.tsx
+++
@@ -1,9 +0,0 @@
-import TestTimeDisplay from '@/components/test-time-display';
-
-export default function TestTimePage() {
-    return (
-        <div className="container mx-auto p-4">
-            <TestTimeDisplay />
-        </div>
-    );
-}
